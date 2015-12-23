1a388a7eb3ba5c9525bdfeb0d7ce1dea0f0c2b40
Matthias Meier <matthias@Matthiass-MBP.fritz.box>
2015-12-23 17:55:57 +0100

    new dev branch

--- 4chanImage/4chanImage/ViewController.swift
+++ 4chanImage/4chanImage/ViewController.swift
@@ -9,10 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var image: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let img = UIImage(named: "4chan.png")
+        image = UIImageView(image: img)
+        
+        
+        
+      //  image.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        view.addSubview(image)
     }
 
     override func didReceiveMemoryWarning() {
