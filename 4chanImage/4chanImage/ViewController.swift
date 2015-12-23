f2e5f19e977b658ef300234688c4a67ae796fd80
Matthias Meier <matthias@Matthiass-MBP.fritz.box>
2015-12-23 15:32:49 +0100

    created new swift ios project

---
+++ 4chanImage/4chanImage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  4chanImage
+//
+//  Created by Matthias Meier on 23.12.15.
+//  Copyright © 2015 Matthias Meier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
